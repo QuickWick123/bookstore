671f20f528483099cefeb17ef74c2320202c63f6
Jermel Stuart <stuartjermel@yahoo.com>
2021-02-22 19:33:59 -0500

    attempt 4

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,8 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.user.username} Profile'
+class MyModel(models.Model):
+    fullname = models.CharField(max_length=200)
+     mobile_number = models.IntegerField()
