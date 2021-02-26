31972362f7e709fb03bd4baacb870a9c433ccb86
houyhourung <houyhourung88@gmail.com>
2021-02-26 13:12:24 -0500

    added sort feature page

--- API/API/settings.py
+++ API/API/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bookstoredb',
         'USER': 'postgres',
-        'PASSWORD': 'test',
+        'PASSWORD': 'cenproject',
         'HOST': 'localhost',
         'PORT': '5432'
     }
--- sort/views.py
+++ sort/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sort(request):
-    return render(request, 'pages/sort.html')
+    response = requests.get('http://localhost:5000/books')
+    book = response.json()
+    book1 = book[1]
+    return render(request, 'pages/sort.html', {"book": book1})
