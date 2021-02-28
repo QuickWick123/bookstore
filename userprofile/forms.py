bc1adc7ac047065c9e9a710ef748de9716a50a47
Jermel Stuart <stuartjermel@yahoo.com>
2021-02-28 09:48:17 -0500

    forms change

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -1,6 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
- 
 class SignUpForm(UserCreationForm):
-    class Meta:
-        model = Profile
-        fields = ('username', 'password1', 'password2', )
+    username = forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    first_name = forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), max_length=32, help_text='First name')
+    last_name=forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), max_length=32, help_text='Last name')
+    email=forms.EmailField(forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), max_length=64, help_text='Enter a valid email address')
+    password1=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # I've tried both of these 'fields' declaration, result is the same
+        # fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
