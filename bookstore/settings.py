b3ee89d1d65f7cbe3e0d3e86373c5712808ca120
Jermel Stuart <stuartjermel@yahoo.com>
2021-02-28 09:28:05 -0500

    forms.py change

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bookstoredb',
         'USER': 'postgres',
-        'PASSWORD': 'test',
+        'PASSWORD': 'Hiddenmist1@',
         'HOST': 'localhost',
         'PORT': '5432'
     }
