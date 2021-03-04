17c84d9dc077c94718f0fd3a17dbed3d295ac52c
houyhourung <houyhourung88@gmail.com>
2021-03-04 17:29:01 -0500

    create sort page layout details

--- sort/views.py
+++ sort/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sort(request):
-    return render(request, 'pages/sort.html')
-    # response = requests.get('http://localhost:5000/books')
-    # book = response.json()
-    # book1 = book[1]
-    # return render(request, 'pages/sort.html', {"book": book1})
+   #  return render(request, 'pages/sort.html')
+    response = requests.get('http://localhost:5000/book')
+    book = response.json()
+    book1 = book[0]
+    return render(request, 'pages/sort.html', {"book": book1})
