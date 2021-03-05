eec7e063a6b01e2074f71199db7acafbbea1cfbc
houyhourung <houyhourung88@gmail.com>
2021-03-04 20:17:46 -0500

    Grab book from  API is working now

--- sort/views.py
+++ sort/views.py
@@ -1,10 +1,28 @@
 import requests
+from bookstore_api.models import Book
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def sort(request):
-   #  return render(request, 'pages/sort.html')
+   #   return render(request, 'pages/sort.html')
     response = requests.get('http://localhost:5000/book')
-    book = response.json()
-    book1 = book[0]
-    return render(request, 'pages/sort.html', {"book": book1})
+    books = Book.objects
+    
+    if 'q' in request.GET:
+      q = request.GET['q']
+      searchResult= Book.objects.filter(name__icontains=q)
+    else:
+       searchResult= Book.objects.all()
+    return render(request, 'pages/sort.html', {"books": books})
+
+
+
+# class SearchResult(ListView):
+#    model = Book
+#    template_name = 'pages/sort.html'
+
+#    def get_queryset(self):
+#       query = query.self.request.GET.get('q')
+#       object_list = Book.objects.fitler(name__contains='')
