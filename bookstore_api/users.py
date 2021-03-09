3b99ad1701687e2b1641506e0032a01349dc03c4
fvela009 <fvela009@fiu.edu>
2021-03-09 11:38:49 -0500

    wishlist

--- bookstore_api/users.py
+++ bookstore_api/users.py
@@ -1,23 +1,74 @@
-from django.shortcuts import render
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status, generics, mixins
+from rest_framework.views import APIView
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import UserProfiles
 from .serializers import UserSerializer
-from rest_framework.decorators import api_view
 
-@api_view(['GET', 'POST', 'DELETE'])
-def userInfo(request):
-    # GET list of tutorials, POST a new tutorial, DELETE all tutorials
 
-    if request.method == 'GET':
+class UserGenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
+                         mixins.CreateModelMixin, mixins.UpdateModelMixin,
+                         mixins.RetrieveModelMixin, mixins.DestroyModelMixin):
+    serializer_class = UserSerializer
+    queryset = UserProfiles.objects.all()
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id=None):
+        return self.update(request, id)
+
+
+class UsersAPIView(APIView):
+
+    def get(self, request):
         users = UserProfiles.objects.all()
         serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        userData = JSONParser().parse(request)
-        user_serializer = UserSerializer(data=data)
-        if user_serializer.is_valid():
-            user_serializer.save()
-            return JsonResponse(user_serializer.data, status=status.HTTP_201_CREATED) 
-        return JsonResponse(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class userDetails(APIView):
+
+    def get_object(self, id):
+        try:
+            return UserProfiles.objects.get(id=id)
+
+        except UserProfiles.DoesNotExist:
+            return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+
+    def get(self, request, id):
+        user = self.get_object(id)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        UserProfiles = self.get_object(id)
+        serializer = UserSerializer(UserProfiles, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        UserProfiles = self.get_object(id)
+        UserProfiles.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -3,10 +3,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def shoppingcart(request):
+    import requests
+    from django.shortcuts import render
+    from django.http import HttpResponse
 
-    response = requests.get('http://localhost:8000/book')
-    book = response.json()
-    book1 = book[22]
-    return render(request, 'pages/shoppingcart.html', {"book": book1})
+    def shoppingcart(request):
+        activeUser = 1
+        response = requests.get('http://localhost:8000/userdetail/' + str(activeUser) + "/")
+        userInfo = response.json()
+        cart = userInfo["cart"]  # gets all cart info as list of dicts
+
+        booksInCart = list()
+        for item in cart:
+            response = requests.get('http://localhost:8000/detail/' + str(item['book']) + "/")
+            book = response.json()
+            booksInCart.append(book)
+
+        return render(request, 'pages/shoppingcart.html', {"cartItems": cart, "books": booksInCart})
     
 
