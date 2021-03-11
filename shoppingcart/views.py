ea865f8378acfe358dfcff07a778c057d9e08a2a
Marjorie <msuar047@fiu.edu>
2021-03-10 22:37:33 -0500

    Display book Quantity

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -4,17 +4,24 @@ from django.http import HttpResponse
 
 
 def shoppingcart(request):
-    activeUser = 1
+    activeUser = 2 #two users 1/2
     response = requests.get('http://localhost:8000/userdetail/'+str(activeUser)+"/")
     userInfo = response.json()
     cart = userInfo["cart"] #gets all cart info as list of dicts
    
+    # bookQuantities = userInfo["cart"]["quantity"]
+    # print(bookQuantities)
+    # bookQlist = list()
+    # for key in cart:
+    #     for quantity in cart["quantity"]
+    #     print(quantity)
 
-    booksInCart = list()
+    i=0
     for item in cart:
         response = requests.get('http://localhost:8000/detail/' + str(item['book']) + "/")
         book = response.json()
-        booksInCart.append(book)
+        cart[i]["book"] = book
+        i += 1
  
-
-    return render(request, 'pages/shoppingcart.html', {"cartItems": cart, "books": booksInCart})
+    print(cart)
+    return render(request, 'pages/shoppingcart.html', {"cartItems": cart})
