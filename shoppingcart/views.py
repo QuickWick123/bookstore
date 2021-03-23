a29892746341a07719b938e750a138c115b5ab15
Marjorie <msuar047@fiu.edu>
2021-03-22 22:57:25 -0400

    Cart total added

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 def shoppingcart(request):
 
-    return render(request, 'pages/shoppingcart.html', {"cartItems": buildData()})
+    return render(request, 'pages/shoppingcart.html', {"cartItems": buildData(), "cartTotal": cartTotal()})
 
 
 def deleteItem(request, book_id):
@@ -15,7 +15,6 @@ def deleteItem(request, book_id):
     print("resp", response)
 
     return shoppingcart(request)
-
 
 def buildData():
     activeUser = 1 #two users 1/2
@@ -30,3 +29,25 @@ def buildData():
         cart[i]["book"] = book
         i += 1
     return cart
+
+def cartTotal():
+    activeUser = 1 #two users 1/2
+    response = requests.get('http://localhost:8000/userdetail/'+str(activeUser)+"/")
+    userInfo = response.json()
+    cart = userInfo["cart"] 
+
+
+    i = 0
+    total = 0
+    for item in cart:
+        response = requests.get('http://localhost:8000/detail/' + str(item['book']) + "/")
+        book = response.json()
+        # cart[i]["book"] = book['price'] UNNECESSARY?
+        item = book['price']
+        quantity = cart[i]["quantity"]
+        subTotal = item*quantity
+        total = subTotal + total
+        #print(quantity, item, subTotal, cartTotal)
+        i += 1
+        
+    return total
