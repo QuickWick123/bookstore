b19cb6c8c6afc88ebf63e37928ce39ce411814f1
houyhourung <houyhourung88@gmail.com>
2021-03-22 20:21:36 -0400

    Added sort by dropdown list

--- sort/urls.py
+++ sort/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import SearchResultsView, browse_results_view, browse_by_category, top_10_sellers, top_20_sellers
 from . import views
 
@@ -8,7 +8,8 @@ urlpatterns = [
     path(r'?P<catetgory>', views.browse_by_category, name='browse_by_category'),
     path(r'', views.browse_results_view, name='BroweseResultsView'),
     path(r'top10sellers', views.top_10_sellers, name='top_10_sellers'),
-    path(r'top20sellers', views.top_20_sellers, name='top_20_sellers'),
+    re_path(r'^top20sellers/(?P<page>[0-9])/', views.top_20_sellers, name='top_20_sellers'),
+    path(r'top20sellers/', views.top_20_sellers, name='top_20_sellers'),
     path('searchView/', views.searchview, name= 'searchView'),
     path('search/', SearchResultsView.as_view(), name='SearchResultsView')
   
--- sort/views.py
+++ sort/views.py
@@ -9,20 +9,6 @@ from django.core.paginator import Paginator
 
 
 
-# def sort(request):
-#    #   return render(request, 'pages/sort.html')
-#     response = requests.get('http://localhost:8000/book')
-#     books = Book.objects
-#     return render(request, 'pages/sort.html', {"books": books})
-
-# def get_queryset(request):
-#    query = request.get('http://localhost:8000/book')
-#    results = Book.objects.filter(
-#       Q(title__icontains=query) | Q(authors__icontains=query)
-#       )
-  
-#    return render(request, 'pages/sort.html', {'results': results})
-
 TEMPLATE_NAME = "pages/sort.html"
 
 class SearchResultsView(ListView):
@@ -37,14 +23,15 @@ class SearchResultsView(ListView):
         )
       return object_list
 
-   def listing(self):
-    books = Book.objects.all()
-    paginator = Paginator(books, 2) # Show 25 contacts per page.
+   # def listing(self):
+   #  books = Book.objects.all()
+   #  paginator = Paginator(books, 2) # Show 25 contacts per page.
+   #  page_number = self.request.GET.get('page')
+   #  page_obj = paginator.get_page(page_number)
+   #  return render(self.request, 'list.html', {'page_obj': page_obj})
 
-    page_number = self.request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(self.request, 'list.html', {'page_obj': page_obj})
-
+def searchview(request):
+   return render(request, 'pages/sort.html')
 
 def browse_results_view(request):
    book = Book.objects.all()
@@ -61,34 +48,15 @@ def top_10_sellers(request):
    top_10_list = Book.objects.all()
    return render(request, TEMPLATE_NAME, {'top_10_list': top_10_list})
 
+
 def top_20_sellers(request):
    top_20_list = Book.objects.all()
-   return render(request, TEMPLATE_NAME, {'top_20_list': top_20_list})
+   top_20_list_paginator = Paginator(top_20_list, 10) # Show 25 contacts per page.
+   page_num = request.GET.get('page')
+   page = top_20_list_paginator.get_page(page_num)
+   context = {
+      'count' : top_20_list_paginator.count,
+      'page' : page
+   }
+   return render(request, TEMPLATE_NAME, context)
 
-
-#  obj = requests.get('http://localhost:8000/book')
-#    for category in obj: 
-#       categories[category["categories"]] = category
-
-# class BrowseResultsView(ListView):
-#    model = Book
-#    template_name = TEMPLATE_NAME
-#    def get_queryset(self): # new
-#      #query = self.request.GET.get('q')
-#       query = self.request.GET.get('catetgory')
-#       object_list = Book.objects.filter(
-#             Q(catetgory__icontains=query))
-#       return object_list
-  
-   # def get_queryset(self):
-   #    query = query.self.request.GET.get('q')
-   #    object_list = Book.objects.fitler(name__contains='')
-
-#  if 'q' in request.GET:
-#       q = request.GET['q']
-#       searchResult= Book.objects.filter(name__icontains=q)
-#     else:
-#        searchResult= Book.objects.all()
-
-def searchview(request):
-   return render(request, 'pages/sort.html')
