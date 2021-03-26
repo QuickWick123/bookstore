738edf823737cf24d6c37a7c49bb377d756284aa
Marjorie <msuar047@fiu.edu>
2021-03-26 13:35:22 -0400

    Update Cart API + Button work

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -16,6 +16,14 @@ def deleteItem(request, book_id):
 
     return shoppingcart(request)
 
+def updateQuantity(request, book_id):
+    activeUser = 1
+    quantityButton = int(request.GET.get('q')) #returns form quantity
+    response = requests.patch('http://localhost:8000/usercart/'+str(activeUser)+"/", data = {"book": book_id, "quantity": quantityButton})
+
+    print(quantityButton)
+    return shoppingcart(request)
+
 def buildData():
     activeUser = 1 #two users 1/2
     response = requests.get('http://localhost:8000/userdetail/'+str(activeUser)+"/")
@@ -41,12 +49,10 @@ def cartTotal():
     for item in cart:
         response = requests.get('http://localhost:8000/detail/' + str(item['book']) + "/")
         book = response.json()
-        # cart[i]["book"] = book['price'] UNNECESSARY?
-        item = book['price']
-        quantity = cart[i]["quantity"]
+        item = int(book['price'])
+        quantity = int(cart[i]["quantity"])
         subTotal = item*quantity
         total = subTotal + total
-        #print(quantity, item, subTotal, cartTotal)
         i += 1
         cartTotalFormatted = "{:.2f}".format(total)
         
