3455ea0352e3ddd8f52c958b7299a8bc33949ff5
Quickwick123 <gameregistery@hotmail.com>
2021-03-30 21:00:57 -0400

    1

--- bookstore_api/serializers.py
+++ bookstore_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Book, UserProfiles, Cart, Comments, Latercart, Orderplaced, Wishlist
+from .models import Book, UserProfiles
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -11,29 +11,4 @@ class BookSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfiles
-        fields = '__all__'
-
-class CartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Cart
-        fields = '__all__'
-
-class CommentsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comments
-        fields = '__all__'
-
-class LatercartSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Latercart
-        fields = '__all__'
-
-class OrderplacedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Orderplaced
-        fields = '__all__'
-
-class WishlistSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Wishlist
         fields = '__all__'
