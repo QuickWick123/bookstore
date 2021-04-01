4f70a7c91be6a67ee11f0ab755e348296e0a54ad
Marjorie <msuar047@fiu.edu>
2021-03-31 22:14:57 -0400

    Fixed conflicting 'deleteItem' button.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
-    path('wishlist/d/<int:book_id>', views.deleteItem, name='deleteItem'),
+    path('wishlist/d/<int:book_id>', views.deleteItem, name='deleteItem2'),
     path('wishlist/add/', views.add, name='add'),
     path('wishlist/a/<int:book_id>', views.addCart, name='addCart'),
 ]
