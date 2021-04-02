0015328268dbac75d07bce03171543601451ef16
Jermel Stuart <stuartjermel@yahoo.com>
2021-04-02 19:08:36 -0400

    userprofile fix

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.userprofile, name='userprofile')
+    path('', views.userprofile, name='userprofile'),
+    path('signup/', views.signup, name='signup'),
 ]
